21d6d80f856b3ff6879be204a7736ee74d5af881
igormendes1997 <igor19_@outlook.com>
2024-03-24 19:39:20 -0300

    adicionei estrutura de decisão e operador de comparação

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 let nomeHeroi = "Hobbs"
- let experienciaXP = 5000
-let nivel;     
+let experienciaXP = 5000
+let nivel  
+
+  if (experienciaXP <5001) { 
+         nivel = "Prata"
+
+  }
+
+   else { 
+    nivel = "Ouro" 
+
+   }
+
+   console.log ( " o heroi de nome " + nomeHeroi + "está no nivel " + nivel );
