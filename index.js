0b46af7636fa45e3415d0fe651003d80eb58ebdb
igormendes1997 <igor19_@outlook.com>
2024-03-24 19:08:15 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let nomeHeroi= "Hobbs" 
+   let experienciaXP = 5000
+   let nivel
+
+   if(experienciaXP <5001){
+          nivel = "Prata"
+        }
+
+    else{
+        nivel = "Ouro"
+    }    
+console.log (" o heroi de nome " + nomeHeroi + " está no nivel " + nivel )
