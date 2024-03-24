ac272e2a46a3fd5b433b3bcc1f9d64201e18a804
igormendes1997 <igor19_@outlook.com>
2024-03-24 19:24:19 -0300

    adicionei variaveis de tipo String e tipo Number

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-let nomeHeroi= "Hobbs" 
-   let experienciaXP = 5000
-   let nivel
-
-   if(experienciaXP <5001){
-          nivel = "Prata"
-        }
-
-    else{
-        nivel = "Ouro"
-    }    
-console.log (" o heroi de nome " + nomeHeroi + " está no nivel " + nivel )
+let nomeHeroi = "Hobbs"
+ let experienciaXP = 5000
+let nivel;     
